c61e71efc11867bb556784281b97edcb0e3c8b99
Vishal Gupta <epochbrokers3@gmail.com>
2022-06-22 15:08:24 +0530

    insurer and blog add on packgae step 1 page

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,27 +1,31 @@
 import Link from "next/link";
 
-const Index = () => {
+const Index = ({posts}) => {
     return (
-        <div>
-            <Link href="/profile" ><a>Profile</a></Link>
-        </div>
+        <ol>
+           {/* {posts.map((post) => (
+                <li>
+                      <Link href={`/blog/${(post.id)}`} passHref>
+                        <a  key={post.id}>{post.title}</a></Link>
+                    </li>
+            ))} */}
+        </ol>
     );
 }
 
 export default Index;
 
 
-// export async function getStaticProps() {
-//     // Call an external API endpoint to get posts.
-//     // You can use any data fetching library
-//     const res = await fetch('https://.../posts')
-//     const posts = await res.json()
-  
-//     // By returning { props: { posts } }, the Blog component
-//     // will receive `posts` as a prop at build time
-//     return {
-//       props: {
-//         posts,
-//       },
-//     }
-//   }
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts.
+    // You can use any data fetching library
+    const res = await fetch('https://fakestoreapi.com/products/')
+    const posts = await res.json()
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+      props: {
+        posts,
+      },
+    }
+  }
